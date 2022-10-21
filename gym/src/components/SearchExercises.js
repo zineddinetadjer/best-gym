a5d8796942fa9ee46bb45fbfc8051ff87370a5f2
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-10-21 20:26:17 +0100

    commit

--- gym/src/components/SearchExercises.js
+++ gym/src/components/SearchExercises.js
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography>
+        Powerfull exercises you <br /> should know
+      </Typography>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
