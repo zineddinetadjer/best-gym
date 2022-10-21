58cd48b7bbf395b6f096f1ad40730307fe399260
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-10-21 20:18:44 +0100

    complete the banner

--- gym/src/App.js
+++ gym/src/App.js
@@ -5,15 +5,17 @@ import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/exercise/:id" element={<ExerciseDetail />}></Route>
       </Routes>
+      <Footer />
     </Box>
   );
 };
